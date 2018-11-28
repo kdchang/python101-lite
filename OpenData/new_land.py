6d26c601c01c7ec2ddc40667067e69682cf278ae
kdchang <kdchang.cc@gmail.com>
2018-11-28 19:22:54 +0800

    Update open data comment

--- OpenData/new_land.py
+++ OpenData/new_land.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 
 # 讀取資料，這邊讀取本期發布之不動產買賣實價登錄批次資料 https://data.gov.tw/dataset/25119：注意資料集可能會更新，但觀念類似
+# 使用 google sheet 觀看，參考資料集 https://docs.google.com/spreadsheets/d/1hCnDQlzyQBScElPe2sGiJmcpGn8DPiQOuKT73rTEbYM/edit#gid=0
 # 該檔案為 utf-8 編碼，使用編碼使用 utf-8 讀取 csv 檔案成 pandas 的 DataFrame 結構
 raw_data_frame = pd.read_csv('./lvr_landAcsv/a_lvr_land_a.csv', encoding='utf-8')
 
