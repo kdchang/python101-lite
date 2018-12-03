de23a33f6925d2f644e95b89926a74366818cf2b
kdchang <kdchang.cc@gmail.com>
2018-12-03 09:03:35 +0800

    Update pytube

--- YoutubeDownloader/index.py
+++ YoutubeDownloader/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pytube import YouTube
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,17 +9,15 @@ def index():
 	filename = request.args.get('filename')
 	return render_template('index.html', filename=filename)
 
+
 @app.route('/submit', methods=['POST'])
 def post_submit():
-	yt = YouTube()
-	url = request.form.get('url')
-	yt.url = url
-	video = yt.get('mp4', '360p')
-	video.download('./')
-	filename = yt.filename
-	print(yt)
-	print(yt.filename)
-	return redirect(url_for('index', filename=filename))
+    url = request.form.get('url')
+    YouTube(url).streams.first().download()
+    filename = YouTube(url).streams.first().default_filename
+    
+    return redirect(url_for('index', filename=filename))
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
